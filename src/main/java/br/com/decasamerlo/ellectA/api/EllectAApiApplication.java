3c82dcb09579ba85c80ea73f50396e853b398eb0
Andre Merlo <casagrandamerlo@gmail.com>
2019-12-16 17:09:56 -0300

    Commit inicial

---
+++ src/main/java/br/com/decasamerlo/ellectA/api/EllectAApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.decasamerlo.ellectA.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EllectAApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EllectAApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/decasamerlo/ellectA/api/EllectAApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.decasamerlo.ellectA.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EllectAApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
