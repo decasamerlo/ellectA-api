395ac1335ad70e74d6d790bb8696aeb2603d0687
Andre Merlo <casagrandamerlo@gmail.com>
2019-12-19 18:58:39 -0300

    criadas migrations para inserção de eleições e cargos

---
+++ src/main/resources/db/migration/V02__inserir_eleicoes.sql
@@ -0,0 +1,3 @@
+INSERT INTO eleicao (nome, data_inicio, data_fim) VALUES ('Eleição Data A', '2019-11-01', '2020-01-01');
+INSERT INTO eleicao (nome, data_inicio, data_fim) VALUES ('Eleição Data B', '2019-11-01', '2019-12-01');
+INSERT INTO eleicao (nome, data_inicio, data_fim) VALUES ('Eleição Data C', '2020-01-01', '2020-02-01');
---
+++ src/main/resources/db/migration/V03__inserir_cargos.sql
@@ -0,0 +1,3 @@
+INSERT INTO cargo (nome) VALUES ('Diretor');
+INSERT INTO cargo (nome) VALUES ('Vice-diretor');
+INSERT INTO cargo (nome) VALUES ('Programador');
