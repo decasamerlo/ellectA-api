02ca45a76b0bbfb6ae0abb195d781cb7390b5c05
Andre <casagrandamerlo@gmail.com>
2019-12-21 17:19:35 -0300

    criada migration para inserir eleitores

---
+++ src/main/resources/db/migration/V05__inserir_eleitores.sql
@@ -0,0 +1,5 @@
+INSERT INTO eleitor (nome, cpf) VALUES ('André Merlo', '070.351.259-57');
+INSERT INTO eleitor (nome, cpf) VALUES ('João Jandre', '134.693.540-81');
+INSERT INTO eleitor (nome, cpf) VALUES ('Marcia Monteiro', '684.524.000-04');
+INSERT INTO eleitor (nome, cpf) VALUES ('Sandra Salésio', '487.684.470-43');
+INSERT INTO eleitor (nome, cpf) VALUES ('Yuri Yankies', '222.013.440-78');
