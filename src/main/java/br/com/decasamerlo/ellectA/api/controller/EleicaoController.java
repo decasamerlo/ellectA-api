639ff09ada1a6fa97368934f26d9495020e956dc
Andre <casagrandamerlo@gmail.com>
2019-12-23 05:09:05 -0300

    criada função para buscar votos de uma eleição

--- src/main/java/br/com/decasamerlo/ellectA/api/controller/EleicaoController.java
+++ src/main/java/br/com/decasamerlo/ellectA/api/controller/EleicaoController.java
@@ -11,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.decasamerlo.ellectA.api.model.Eleicao;
+import br.com.decasamerlo.ellectA.api.model.Voto;
 import br.com.decasamerlo.ellectA.api.repository.EleicaoRepository;
+import br.com.decasamerlo.ellectA.api.repository.VotoRepository;
 
 @RestController
 @RequestMapping("/eleicoes")
@@ -22,6 +25,9 @@ public class EleicaoController {
 	
 	@Autowired
 	private EleicaoRepository eleicaoRepository;
+	
+	@Autowired
+	private VotoRepository votoRepository;
 	
 	@GetMapping
 	public List<Eleicao> listar() {
@@ -32,6 +38,11 @@ public class EleicaoController {
 	public ResponseEntity<Eleicao> criar(@Valid @RequestBody Eleicao eleicao) {
 		Eleicao eleicaoSalva = eleicaoRepository.save(eleicao);
 		return ResponseEntity.status(HttpStatus.CREATED).body(eleicaoSalva);
+	}
+
+	@GetMapping(params = "relatorio")
+	public List<Voto> gerarRelatorio(@RequestParam(required=true) Long id) {
+		return votoRepository.findByProtocoloEleicaoId(id);
 	}
 
 }
--- src/main/java/br/com/decasamerlo/ellectA/api/repository/VotoRepository.java
+++ src/main/java/br/com/decasamerlo/ellectA/api/repository/VotoRepository.java
@@ -1,10 +1,14 @@
 package br.com.decasamerlo.ellectA.api.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.decasamerlo.ellectA.api.model.Voto;
 
 public interface VotoRepository extends JpaRepository<Voto, Long> {
-
+	
+	List<Voto> findByProtocoloEleicaoId(Long idEleicao);
+	
 }
